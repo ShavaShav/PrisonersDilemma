6f84f073926f54dca993222490fd2e9c091a7a21
Zach Shaver <zachshaver@gmail.com>
2017-03-12 00:45:59 -0500

    Delete SingleLookup.java

--- src/SingleLookup.java
+++
@@ -1,6 +0,0 @@
-
-public interface SingleLookup extends Lookup {
-	public abstract double getAction(History history);
-	public abstract void flipAction(History history);
-	public abstract void setAction(History history, double action);
-}
