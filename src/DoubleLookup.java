f987613c5741968d09c3686fa5e6b28465bbfabb
Zach Shaver <zachshaver@gmail.com>
2017-03-12 00:45:05 -0500

    Delete DoubleLookup.java

--- src/DoubleLookup.java
+++
@@ -1,6 +0,0 @@
-
-public interface DoubleLookup extends Lookup{
-	public abstract double getAction(DoubleHistory doubleHistory);
-	public abstract void flipAction(DoubleHistory doubleHistory);
-	public abstract void setAction(DoubleHistory doubleHistory, double action);
-}
